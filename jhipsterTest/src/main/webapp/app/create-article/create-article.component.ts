4ba703abdcc001ddeb0d8cae908504e848553032
0Kain <y12eg96@gmail.com>
2018-11-27 09:47:09 +0100

    link back front

--- jhipsterTest/src/main/webapp/app/create-article/create-article.component.ts
+++ jhipsterTest/src/main/webapp/app/create-article/create-article.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpResponse, HttpHeaders, HttpParams } from '@angular/common/http';
+import { SERVER_API_URL } from 'app/app.constants';
 
 @Component({
     selector: 'jhi-create-article',
@@ -7,13 +8,32 @@ import { HttpClient, HttpResponse, HttpHeaders, HttpParams } from '@angular/comm
     styles: []
 })
 export class CreateArticleComponent implements OnInit {
-    constructor(private http: HttpClient) {}
-
-    /*this.http.post<any>(path,params).subscribe((response)=>if(response){
-        //magnifique bandeau vert ça marche
-    }else{
-        //bandeau rouge ça marche pas moche
-    })*/
+    constructor(private http: HttpClient) {
+        /*const btn = document.getElementById('createButton');
+        btn.addEventListener('click', (e: Event) => this.validateArticle() );*/
+    }
 
     ngOnInit() {}
+
+    validateArticle() {
+        const title = (document.getElementById('title') as HTMLInputElement).value;
+        const topics = (document.getElementById('topics') as HTMLInputElement).value;
+        const content = (document.getElementById('content') as HTMLInputElement).value;
+        let params = new HttpParams()
+            .set('title', title)
+            .set('topics', topics)
+            .set('content', content);
+        this.http.post<any>(SERVER_API_URL + '/api/articles/create', { params: params }).subscribe(response => {
+            alert(response);
+            if (response) {
+                // bandeau vert ça marche magnifique
+                (document.getElementById('createSuccess') as HTMLInputElement).style.display = 'block';
+                alert('ça marche');
+            } else {
+                alert('ça marche pas');
+                // bandeau rouge ça marche pas moche
+                (document.getElementById('createError') as HTMLInputElement).style.display = 'block';
+            }
+        });
+    }
 }
