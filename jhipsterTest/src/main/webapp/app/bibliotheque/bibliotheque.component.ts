ec1f6e4c0761bbce6ce31acef7c64eafb3d7c746
unknown <quentin.dugne@orange.fr>
2018-12-04 09:41:02 +0100

    Biblio

--- jhipsterTest/src/main/webapp/app/bibliotheque/bibliotheque.component.ts
+++ jhipsterTest/src/main/webapp/app/bibliotheque/bibliotheque.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActuBoxModel } from '../entities/actu-box.model';
 
 @Component({
     selector: 'jhi-bibliotheque',
     templateUrl: './bibliotheque.component.html',
-    styles: []
+    styleUrls: ['bibliotheque.component.scss']
 })
 export class BibliothequeComponent implements OnInit {
-    constructor() {}
-
+    http: HttpClient;
+    infosActus: Array<ActuBoxModel>;
+    constructor(private _http: HttpClient) {
+        this.http = _http;
+        this.http.get('http://localhost:9000/data.json').subscribe((result: Array<ActuBoxModel>) => {
+            //observable/promise : s'abonner à un event et on attend la reception des données et on peut faire un traitement après
+            this.infosActus = result;
+        });
+    }
     ngOnInit() {}
 }
