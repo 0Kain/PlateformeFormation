a2a883561f62deb03a82d412a938927b5f8d93b0
Valentin Courtois <valentin.courtois1006@laposte.net>
2019-02-04 22:19:25 +0100

    preparing href for article consultation

--- jhipsterTest/src/main/webapp/app/app-routing.module.ts
+++ jhipsterTest/src/main/webapp/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { DEBUG_INFO_ENABLED } from './app.constants';
 import { BibliothequeComponent } from './bibliotheque/bibliotheque.component';
 import { CreateArticleComponent } from './create-article/create-article.component';
 import { CoursComponent } from './bibliotheque/cours/cours.component';
+import { ArticleComponent } from 'app/article/article.component';
 
 const LAYOUT_ROUTES = [navbarRoute, ...errorRoute];
 
@@ -15,7 +16,8 @@ const LAYOUT_ROUTES = [navbarRoute, ...errorRoute];
                 ...LAYOUT_ROUTES,
                 { path: 'admin', loadChildren: './admin/admin.module#PlatformationAdminModule' },
                 { path: 'bibliotheque', component: BibliothequeComponent },
-                { path: 'create-article', component: CreateArticleComponent }
+                { path: 'create-article', component: CreateArticleComponent },
+                { path: 'article', component: ArticleComponent }
             ],
             { useHash: true, enableTracing: DEBUG_INFO_ENABLED }
         )
--- jhipsterTest/src/main/webapp/app/home/actu-box/actu-box.component.ts
+++ jhipsterTest/src/main/webapp/app/home/actu-box/actu-box.component.ts
@@ -9,21 +9,9 @@ import { HttpClient } from '@angular/common/http';
     styleUrls: ['actu-box.component.scss']
 })
 export class ActuBoxComponent implements OnInit {
-    //private infoActu: ActuBoxModel;
-
     @Input() private actuBox: ActuBoxModel;
     private infoActu: ActuBoxModel;
-    constructor() {
-        // Je mets à disposition le service au component
-        //parametre sont chiffrés dans l'url
-        /* OldVersion without data
-        const actuCategs: Array<ActuBoxCategorieModel> = [];
-        actuCategs.push(new ActuBoxCategorieModel('informatique'));
-        actuCategs.push(new ActuBoxCategorieModel('pain'));
-        actuCategs.push(new ActuBoxCategorieModel('patisserie'));
-
-        this.infoActu = new ActuBoxModel('Boulangerie', actuCategs, 'Allez les couch on se bouge le fion', '21/10/2018');*/
-    }
+    constructor() {}
 
     ngOnInit() {
         this.infoActu = this.actuBox;
